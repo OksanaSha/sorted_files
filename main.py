141e579d8ecded6f83f565d4e4502666749e36db
OksanaSha <oksanashatalova@bk.ru>
2021-09-27 00:12:58 +0300

    write sorted files to new_file; add open_file; changed file_to_string

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-def file_to_string(file_name):
+def find_len_file(file_name):
     with open(file_name) as file:
         text = file.readlines()
-        text_str = file_name + '\n' + str(len(text)) + '\n' + '' \
-                                                              ''.join(text)
-    return len(text), text_str
+    return len(text), file_name
 
-file1 = tuple(file_to_string('1.txt'))
-file2 = tuple(file_to_string('2.txt'))
-file3 = tuple(file_to_string('3.txt'))
+def open_file_read(file_name):
+    with open(file_name) as file:
+        text = file.read()
+    return text
+
+file1 = tuple(find_len_file('1.txt'))
+file2 = tuple(find_len_file('2.txt'))
+file3 = tuple(find_len_file('3.txt'))
 
 all_files = dict(sorted([file1, file2, file3]))
 
-print(all_files)
+with open('new_file.txt', 'w') as file:
+    for len_file, file_name in all_files.items():
+        file.write(f'{file_name}\n'
+                   f'{len_file}\n'
+                   f'{open_file_read(file_name).rstrip()}\n')
 
-# with open('new_file.)
-
-
+with open ('new_file.txt') as file:
+    print(file.read())
